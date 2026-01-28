4a24480f47ee65b54fd416cee74d1e3d3c8737bc
jormeljim-gva <jormeljim@alu.edu.gva.es>
2026-01-28 13:02:50 +0100

    Comienzo de Enunciado1

--- src/main/java/Enunciado1.java
+++ src/main/java/Enunciado1.java
@@ -1,3 +1,9 @@
 public class Enunciado1 {
+    public String asignaPrima(int numEmpleado, String nombreEmpleado, int mesesTrabajo, char directivo) {
+        if(numEmpleado >= 0 && String.valueOf(numEmpleado).length() == 3 && nombreEmpleado.length() >= 1 && nombreEmpleado.length() <= 10 &&
+        mesesTrabajo >= 0 && String.valueOf(mesesTrabajo).length() == 3 && directivo == '+' || directivo == '-'){
 
+        }
+        return "Algo";
+    }
 }
