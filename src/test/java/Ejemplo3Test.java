9819c71b925b7f2ec78a67062d5f1be9a4bf55c9
jormeljim-gva <jormeljim@alu.edu.gva.es>
2026-01-28 12:38:25 +0100

    Ejemplo3Test completo

--- src/test/java/Ejemplo3Test.java
+++ src/test/java/Ejemplo3Test.java
@@ -10,11 +10,32 @@ class Ejemplo3Test {
         assertEquals(6, new Ejemplo3().calcula(12, 6, '-'));
         assertEquals(72, new Ejemplo3().calcula(12, 6, '*'));
         assertEquals(2, new Ejemplo3().calcula(12, 6, '/'));
-
     }
+
+    @Test
+    void calculaLimitesValidos() {
+        assertEquals(2, new Ejemplo3().calcula(1, 1, '*'));
+        assertEquals(0, new Ejemplo3().calcula(2, 2, '-'));
+        assertEquals(0, new Ejemplo3().calcula(999, 999, '/'));
+        assertEquals(0, new Ejemplo3().calcula(998, 998, '*'));
+    }
+
     @Test
     void calculaInvalidos() {
-        assertThrows(IllegalArgumentException.class, () -> new Ejemplo3().calcula(1001, -1, '+'));
+        assertThrows(IllegalArgumentException.class,() -> new Ejemplo3().calcula(-4, 5, '+'));
+        assertThrows(IllegalArgumentException.class,() -> new Ejemplo3().calcula(1002, 5, '+'));
+        assertThrows(IllegalArgumentException.class,() -> new Ejemplo3().calcula(6, -5, '+'));
+        assertThrows(IllegalArgumentException.class,() -> new Ejemplo3().calcula(6, 1222, '+'));
+        assertThrows(IllegalArgumentException.class,() -> new Ejemplo3().calcula(6, 5, '?'));
+    }
+
+    @Test
+    void calculaLimitesInvalidos() {
+        assertThrows(IllegalArgumentException.class,() -> new Ejemplo3().calcula(0, 1, '+'));
+        assertThrows(IllegalArgumentException.class,() -> new Ejemplo3().calcula(1000, 1, '+'));
+        assertThrows(IllegalArgumentException.class,() -> new Ejemplo3().calcula(1, 0, '+'));
+        assertThrows(IllegalArgumentException.class,() -> new Ejemplo3().calcula(1, 1000, '+'));
+        assertThrows(IllegalArgumentException.class,() -> new Ejemplo3().calcula(1, 1, '?'));
     }
 
 }
