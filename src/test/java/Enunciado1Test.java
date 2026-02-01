da8808ec674919c4e84888a3c109cab9e143a534
Carlitic <carloscastanosblanco@gmail.com>
2026-02-01 11:43:31 +0100

    Enunciado1Test.java acabado todo el Enunciado1 esta acabado

--- src/test/java/Enunciado1Test.java
+++ src/test/java/Enunciado1Test.java
@@ -5,6 +5,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class Enunciado1Test {
 
     @Test
-    void asignaPrima() {
+    void calculaValidos() {
+        assertEquals("P1", new Enunciado1().asignaPrima(120, "fernando", 130, '+'));
+        assertEquals("P2", new Enunciado1().asignaPrima(120, "fernando", 118, '-'));
+        assertEquals("P3", new Enunciado1().asignaPrima(120, "fernando", 108, '+'));
+        assertEquals("P4", new Enunciado1().asignaPrima(120, "fernando", 110, '-'));
+    }
+
+    @Test
+    void calculaLimitesValidos() {
+        assertEquals("P4", new Enunciado1().asignaPrima(100, "a", 100, '-'));
+        assertEquals("P1", new Enunciado1().asignaPrima(999, "aitoraitoo", 999, '+'));
+    }
+
+    @Test
+    void calculaInvalidos() {
+        assertThrows(IllegalArgumentException.class, () -> new Enunciado1().asignaPrima(-2, "fernando", 110, '-'));
+        assertThrows(IllegalArgumentException.class, () -> new Enunciado1().asignaPrima(1002, "fernando", 110, '-'));
+        assertThrows(IllegalArgumentException.class, () -> new Enunciado1().asignaPrima(120, "fernando", -5, '-'));
+        assertThrows(IllegalArgumentException.class, () -> new Enunciado1().asignaPrima(120, "estenombreesmuylargo", 110, '-'));
+        assertThrows(IllegalArgumentException.class, () -> new Enunciado1().asignaPrima(120, "fernando", 110, '?'));
+    }
+
+    @Test
+    void calculaLimitesInvalidos() {
+        assertThrows(IllegalArgumentException.class, () -> new Enunciado1().asignaPrima(100, "", 112, '+'));
+        assertThrows(IllegalArgumentException.class, () -> new Enunciado1().asignaPrima(100, "a", 1000, '+'));
     }
 }
