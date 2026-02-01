a1e06b9d9ea2fe399068658698b874687f1cfa45
Carlitic <carloscastanosblanco@gmail.com>
2026-02-01 10:55:05 +0100

    Enunciado1.java acabado

--- src/main/java/Enunciado1.java
+++ src/main/java/Enunciado1.java
@@ -3,7 +3,21 @@ public class Enunciado1 {
         if(numEmpleado >= 0 && String.valueOf(numEmpleado).length() == 3 && nombreEmpleado.length() >= 1 && nombreEmpleado.length() <= 10 &&
         mesesTrabajo >= 0 && String.valueOf(mesesTrabajo).length() == 3 && directivo == '+' || directivo == '-'){
 
+            if (mesesTrabajo >= 12) {
+                if (directivo == '+') {
+                    return "P1";
+                } else {
+                    return "P2";
+                }
+            } else {
+                if (directivo == '+') {
+                    return "P3";
+                } else {
+                    return "P4";
+                }
+            }
+
         }
-        return "Algo";
+        else throw new IllegalArgumentException("ERROR");
     }
 }
